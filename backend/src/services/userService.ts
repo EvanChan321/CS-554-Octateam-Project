c6f951e89f1c4728d6ee3ec1b74689d806f23e44
octavio-morales1 <118334340+octavio-morales1@users.noreply.github.com>
2024-12-12 23:09:43 -0500

    Major revamp

--- backend/src/services/userService.ts
+++
@@ -1,6 +0,0 @@
-import { users } from '../config/mongoCollections';
-
-export const getUser = async (userId: string) => {
-  const usersCollection = await users();
-  return usersCollection.findOne({ _id: userId });
-};

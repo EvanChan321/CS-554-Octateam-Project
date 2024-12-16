64c03b38a3c05e4fd9b0bac5fa7092e5d0b357d6
octavio-morales1 <118334340+octavio-morales1@users.noreply.github.com>
2024-12-16 02:26:59 -0500

    Git Ignore Problems

--- backend/config/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // Place the Firebase private key here
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-module.exports = admin;
